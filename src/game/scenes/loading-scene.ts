e1e8285b3236d3cb844bc9beb9c38c16bbc19d1f
Luiz Felipe <luizfelipedasilva678@gmail.com>
2025-05-25 22:32:27 -0300

    Fix paths

--- src/game/scenes/loading-scene.ts
+++ src/game/scenes/loading-scene.ts
@@ -1,5 +1,11 @@
 import Phaser from "phaser";
 import { Text } from "../components";
+import diceAlbedoUrl from "/dice-albedo.png?url";
+import diceObjUrl from "/dice.obj?url";
+import pawnUrl from "/pawn.png?url";
+import closeUrl from "/close.svg?url";
+import monsterUrl from "/monster.png?url";
+import gameConfigFormUrl from "/html/game-config-form.html?url";
 
 class LoadingScene extends Phaser.Scene {
   constructor() {
@@ -7,12 +13,12 @@ class LoadingScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("dice-albedo", "/dice-albedo.png");
-    this.load.obj("dice-obj", "/dice.obj");
-    this.load.image("pawn", "/pawn.png");
-    this.load.image("close", "/close.svg");
-    this.load.image("monster", "/monster.png");
-    this.load.html("game-config-form", "/html/game-config-form.html");
+    this.load.image("dice-albedo", diceAlbedoUrl);
+    this.load.obj("dice-obj", diceObjUrl);
+    this.load.image("pawn", pawnUrl);
+    this.load.image("close", closeUrl);
+    this.load.image("monster", monsterUrl);
+    this.load.html("game-config-form", gameConfigFormUrl);
 
     new Text(
       this,
