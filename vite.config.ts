94fb873e0e9045de8df71a9d086146e31d6e9bea
Luiz Felipe <luizfelipedasilva678@gmail.com>
2025-05-25 22:24:19 -0300

    Fix CI

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/board-game/",
   build: {
     cssMinify: true,
     minify: "esbuild",
